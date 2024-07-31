3dce958ce6fac069b55f9dda9a0af6bf73f6de94
Jeyson Erik Valdivia Bernal <jasson.n021@gmail.com>
2024-07-31 08:26:09 -0400

    Conditionals

---
+++ COnditionals/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program { 
+    public static void Main(string[] args)
+    {
+        int initial = 0;
+        bool response = true;
+       while (response)
+        {
+            Console.WriteLine("añada un numero");
+            int num = int.Parse(Console.ReadLine());
+            int partialResult = num + initial;
+            initial = partialResult;
+            Console.WriteLine("la suma es: " + partialResult);
+            Console.WriteLine("Desea seguir sumando");
+            bool res=bool.Parse(Console.ReadLine());
+            if (!res) { 
+                response = false;
+                Console.WriteLine("La suma total es: " + partialResult);
+            }
+        }
+    }
+}
