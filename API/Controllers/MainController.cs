f379c9d03adf1d29a30f8438e365a78c0e801eb6
Jeyson Erik Valdivia Bernal <jasson.n021@gmail.com>
2024-08-01 09:59:35 -0400

    API

---
+++ API/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace API.Controllers
+{
+    [Route("api/v1/hello_world")]
+    public class MainController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Persona> Get() 
+        { 
+            Persona persona = new Persona();
+            persona.Name = "Pedro";
+            persona.Age = 30;
+            return Ok(persona);
+        }
+    }
+
+
+    public class Persona 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }  
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuation = configuration;
+        }
+
+        public IConfiguration Configuation { get; set; }
+
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseRouting();
+                app.UseAuthentication();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync("Todo Bien");
+                    });
+                });
+            }
+        }
+    }
+}
